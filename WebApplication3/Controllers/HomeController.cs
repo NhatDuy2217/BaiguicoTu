163d942aa9020b5bdd751220e030cd8784e2bda1
NhatDuy2217 <nhatduy2217@gmail.com>
2026-09-22 10:20:45 +0700

    Lab01a

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bai1()
+        {
+            return View();
+        }
+
+        public IActionResult Bai2()
+        {
+            return View();
+        }
+
+        public IActionResult Bai3()
+        {
+            return View();
+        }
+    }
+}
